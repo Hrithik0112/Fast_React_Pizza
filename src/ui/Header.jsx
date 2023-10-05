38518d60baaeb201f35186a0daf6b4e7ed5ca816
Hrithik <slumdog4321@gmail.com>
2023-10-05 10:59:05 +0530

    search oder id component added

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
 
 function Header() {
   return (
     <header>
       <Link to="/">Fast React Pizza Co.</Link>
+      <SearchOrder />
       <p>Hrithik</p>
     </header>
   );
