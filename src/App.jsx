04093c3c53079680d5cb067e55eddbfb31d91f9d
Hrithik <slumdog4321@gmail.com>
2023-10-05 11:35:13 +0530

    fromaction using react router, create & post order

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from "./ui/Home";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import Order, { loader as orderLoader } from "./features/order/Order";
-import CreateOrder from "./features/order/CreateOrder";
+import CreateOrder, { action as createOrderAction } from "./features/order/CreateOrder";
 import AppLayout from "./ui/AppLayout";
 import NotFound from "./ui/Error";
 
@@ -16,7 +16,7 @@ const router = createBrowserRouter([
       { path: "/", element: <Home /> },
       { path: "/menu", element: <Menu />, loader: menuLoader, errorElement: <NotFound /> },
       { path: "/cart", element: <Cart /> },
-      { path: "/order/new", element: <CreateOrder /> },
+      { path: "/order/new", element: <CreateOrder />, action: createOrderAction },
       {
         path: "/order/:orderId",
         element: <Order />,
