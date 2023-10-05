124cb9650202d094d55ef108c5a07508a259705d
Hrithik <slumdog4321@gmail.com>
2023-10-05 18:08:19 +0530

    some minor fixes

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold md:block">Jonas</div>;
+  return <div className="hidden text-sm font-semibold md:block">Hrithik</div>;
 }
 
 export default Username;
--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,3 +1,5 @@
+import CreateUser from "../features/user/CreateUser";
+
 function Home() {
   return (
     <div className="my-10 px-4 text-center sm:my-16">
@@ -6,6 +8,7 @@ function Home() {
         <br />
         <span className="text-yellow-500">Straight out of the oven, straight to you.</span>
       </h1>
+      <CreateUser />
     </div>
   );
 }
