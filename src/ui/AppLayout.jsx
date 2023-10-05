84c59d444cb19a42662ad964fc78b8f34d950062
Hrithik <slumdog4321@gmail.com>
2023-10-05 17:46:04 +0530

    update ui components and font added

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -8,12 +8,15 @@ function AppLayout() {
   const navigation = useNavigation();
   const isLoading = navigation.state === "loading";
   return (
-    <div className="layout">
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       {isLoading && <Loader />}
       <Header />
-      <main>
-        <Outlet />
-      </main>
+
+      <div className="overflow-scroll">
+        <main className="mx-auto max-w-3xl">
+          <Outlet />
+        </main>
+      </div>
       <CartOverview />
     </div>
   );
--- src/ui/Loader.jsx
+++ src/ui/Loader.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Loader() {
-  return <div className="loader">Loader</div>;
+  return (
+    <div className="absolute inset-0 flex items-center justify-center bg-slate-200/20 backdrop-blur-sm">
+      <div className="loader"></div>
+    </div>
+  );
 }
 
 export default Loader;
